d029d318fba59715a06a01b6b122d5f339fc4efb
Jean-Jacob Klat <jj@Jean-Jacobs-MacBook-Pro.local>
2022-10-19 23:50:00 +0200

    finished precourse material

---
+++ dictonaries.py
@@ -0,0 +1,25 @@
+#%%
+
+
+beatles = {
+    "bass": "Paul",
+    "guitar": "George",
+    "vocals": "John",
+    "drums": "Ringo"
+}
+
+for key in beatles:
+    print(beatles[key] + " plays" + key + "in the beatles")
+
+#%% empty_dict = {}
+empty_dict[]
+# %%
+
+with open("hello.txt", "w") as file: 
+    file.writelines()
+
+    #%% 
+    
+    with open("program.csv") as file:
+        for line in file: 
+            print(line)
