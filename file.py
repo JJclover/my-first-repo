b5a25c0c21d549df098a06d6f98cf6f472afd313
Jean-Jacob Klat <jj@Jean-Jacobs-MacBook-Pro.local>
2022-10-23 14:45:04 +0200

    changed to addition again

--- file.py
+++ file.py
@@ -1,8 +1,8 @@
 def tomato(a, b):
-    return a - b 
+    return a + b 
 
 
-print(tomato(9,2))
+print(tomato(1,2))
 
 #next time continue with this when pushing and pulling, 
 #in git hub account there should not be a print 
